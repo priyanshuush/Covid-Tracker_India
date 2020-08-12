5469ce9f88e0c14ad38157c5619e896783860ed9
YOYO <xyz@gmail.com>
2020-08-12 21:07:02 +0530

    Updates

---
+++ precache-manifest.bad2ae9c24222996feff5a71dc130fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b32a86605c3001161f48e9684d77dd7",
+    "url": "/Covid-Tracker_India/index.html"
+  },
+  {
+    "revision": "ffc843c40df07238eb16",
+    "url": "/Covid-Tracker_India/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "d1d301427e7492d119e2",
+    "url": "/Covid-Tracker_India/static/js/2.889bb562.chunk.js"
+  },
+  {
+    "revision": "4c5fffd6831b7d539642ad256ec5cb71",
+    "url": "/Covid-Tracker_India/static/js/2.889bb562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc843c40df07238eb16",
+    "url": "/Covid-Tracker_India/static/js/main.26840ca3.chunk.js"
+  },
+  {
+    "revision": "07563c28b24d1b5f8620",
+    "url": "/Covid-Tracker_India/static/js/runtime-main.488ec0d4.js"
+  },
+  {
+    "revision": "dc62b3fa0d7735391d736f6c383590bd",
+    "url": "/Covid-Tracker_India/static/media/logo.dc62b3fa.svg"
+  }
+]);
